c5b0636a0de0ad55988999d81719e126849b026d
Martina Andersson <ma225gc@student.lnu.se>
2022-05-11 13:45:20 +0200

    Handle invalid input

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,29 +1,48 @@
 import React, { Component } from "react"
 
 class Form extends Component {
-  toggleDropdown() {
-    document.getElementById("country-list").classList.toggle("show")
-    console.log('Display dropdown')
+  constructor(props) {
+    super(props)
+    this.state = {
+       name: '',
+       weight: null,
+       color: '#FFFFFF',
+       country: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e) {
+    for (const value in this.state) {
+      if (this.state[value] === 'undefined' || this.state[value] === null) {
+        alert('You must enter a valid value for ' + value)
+        return
+      }
+    }
+
+    console.log(this.state)
+    e.preventDefault()
   }
 
   render() {
     return (
       <div id="form-container">
-        <form id="form">
+        <form id="form" onSubmit={this.handleSubmit}>
           <label htmlFor="box-name">Name:</label><br/>
-          <input type="text" id="box-name" name="box-name"/><br/>
+          <input type="text" id="box-name" name="box-name" onChange={e => this.setState({name: e.target.value})}/><br/>
           <label htmlFor="box-weight">Weight:</label><br/>
-          <input type="text" id="box-weight" name="box-weight"/><br/>
+          <input type="number" id="box-weight" name="box-weight" onChange={e => this.setState({weight: e.target.value})}/><br/>
           <label htmlFor="box-colour">Box colour:</label><br/>
-          <input type="color" id="box-colour" name="box-colour" value="#FFFFFF"/><br/>
+          <input type="color" id="box-colour" name="box-colour" onChange={e => this.setState({color: e.target.value})}/><br/>
           <label htmlFor="country">Country:</label><br/>
-          <select id="country-list">
+          <select id="country-list" onChange={e => this.setState({country: e.target.value})}>
             <option value="Sweden">Sweden</option>
             <option value="China">China</option>
             <option value="Brazil">Brazil</option>
             <option value="Australia">Australia</option>
           </select><br/>
-          <button id="save-btn">Save</button>
+          <input type="submit" id="save-btn" placeholder="Save"/>
         </form>
       </div>
     )
